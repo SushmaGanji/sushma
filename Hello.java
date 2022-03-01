8cd9b3335084a5994bc0cf964caa4038b7c57a8c
SushmaGanji <sushma.ganji002@gmail.com>
2022-03-01 15:17:31 +0530

    commiting

--- Hello.java
+++ Hello.java
@@ -2,5 +2,10 @@ public class Hello{
 public static void main(String []args)
 {
 System.out.println("hello world");
+int a=10;
+int b=20;
+int c=30;
+int d=6566;
+s=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
 }
 }
